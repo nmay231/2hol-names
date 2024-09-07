452086602f2fd99d4fa8009db778950a414191db
Noah May <noahmouse2011@gmail.com>
2024-09-07 04:14:07 -0500

    build script and helpful message

--- main.cpp
+++ main.cpp
@@ -5,6 +5,8 @@
 int main() {
     initNames();
 
+    std::cerr << "Pipe in name attempts one per line." << std::endl;
+
     for (std::string line; std::getline(std::cin, line);) {
         const char* output = findCloseFirstName((char*)line.c_str());
         std::cout << output << std::endl;
