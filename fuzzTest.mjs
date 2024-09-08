284187f38181a5c398bb83ff101cf1c774eba553
Noah May <noahmouse2011@gmail.com>
2024-09-07 20:36:02 -0500

    Don't need to run test in IIFE since I load the file differently now

--- fuzzTest.mjs
+++ fuzzTest.mjs
@@ -15,81 +15,76 @@ const randomString = (length) => {
     return result;
 }
 
-(async () => {
+let testNames = [
+    ...Array.from(ALPHABET),
+]
 
-    let testNames = [
-        ...Array.from(ALPHABET),
-    ]
+const firstNames = fs.readFileSync('./docs/firstNames.txt').toString();
+const lastNames = fs.readFileSync('./docs/lastNames.txt').toString();
 
-    const firstNames = fs.readFileSync('./docs/firstNames.txt').toString();
-    const lastNames = fs.readFileSync('./docs/lastNames.txt').toString();
-    
-    const firstNamesList = firstNames.trim().split('\n');
-    const lastNamesList = lastNames.trim().split('\n');
+const firstNamesList = firstNames.trim().split('\n');
+const lastNamesList = lastNames.trim().split('\n');
 
-    const grunge = (name) => {
-        const slice = name.slice(0, random(0, name.length - 1));
-        testNames.push(name, slice, `${slice}${randomString(random(0, 10))}`);
-    }
+const grunge = (name) => {
+    const slice = name.slice(0, random(0, name.length - 1));
+    testNames.push(name, slice, `${slice}${randomString(random(0, 10))}`);
+}
 
-    for (let i = 0; i < 1000; i++) {
-        grunge(firstNamesList[random(0, firstNamesList.length - 1)]);
-        grunge(lastNamesList[random(0, lastNamesList.length - 1)]);
-        testNames.push(randomString(random(0, 10)));
-    }
+for (let i = 0; i < 1000; i++) {
+    grunge(firstNamesList[random(0, firstNamesList.length - 1)]);
+    grunge(lastNamesList[random(0, lastNamesList.length - 1)]);
+    testNames.push(randomString(random(0, 10)));
+}
 
-    for (let i = 0; i < 10; i++) {
-        grunge(firstNamesList[0]);
-        grunge(firstNamesList[firstNamesList.length - 1]);
-        grunge(lastNamesList[0]);
-        grunge(lastNamesList[lastNamesList.length - 1]);
-    }
+for (let i = 0; i < 10; i++) {
+    grunge(firstNamesList[0]);
+    grunge(firstNamesList[firstNamesList.length - 1]);
+    grunge(lastNamesList[0]);
+    grunge(lastNamesList[lastNamesList.length - 1]);
+}
 
-    testNames = testNames.filter((name, index, self) => self.indexOf(name) === index);
+testNames = testNames.filter((name, index, self) => self.indexOf(name) === index);
 
-    console.log(testNames.length);
+console.log(testNames.length);
 
 
-    const child = spawn('./main');
-    const output = [];
-    child.stdout.on('data', (data) => {
-        output.push(...data.toString().trim().split('\n').map(names => {
-            const [first, last] = names.trim().split(':');
-            return { first, last };
-        }));
-    });
-    child.stderr.on('data', (data) => {
-        console.log(data.toString());
-    });
+const child = spawn('./main');
+const output = [];
+child.stdout.on('data', (data) => {
+    output.push(...data.toString().trim().split('\n').map(names => {
+        const [first, last] = names.trim().split(':');
+        return { first, last };
+    }));
+});
+child.stderr.on('data', (data) => {
+    console.log(data.toString());
+});
 
-    for (const name of testNames) {
-        child.stdin.write(`${name}\n`);
-    }
-    child.stdin.end();
+for (const name of testNames) {
+    child.stdin.write(`${name}\n`);
+}
+child.stdin.end();
 
-    child.on('close', () => {
-        console.log("Lengths should be the same:", testNames.length, output.length);
+child.on('close', () => {
+    console.log("Lengths should be the same:", testNames.length, output.length);
 
-        for (let i = 0; i < output.length; i++) {
-            const input = testNames[i];
-            const expected = output[i];
-            const firstIndex = findClosestName(firstNames, input);
-            const firstName = subString(firstNames, firstIndex);
-            const lastIndex = findClosestName(lastNames, input);
-            const lastName = subString(lastNames, lastIndex);
+    for (let i = 0; i < output.length; i++) {
+        const input = testNames[i];
+        const expected = output[i];
+        const firstIndex = findClosestName(firstNames, input);
+        const firstName = subString(firstNames, firstIndex);
+        const lastIndex = findClosestName(lastNames, input);
+        const lastName = subString(lastNames, lastIndex);
 
-            if (firstName !== expected.first) {
-                console.log("Mismatch:", input, "expected =", expected.first, "actual =", firstName);
-                console.log(firstNames.slice(Math.max(0, firstIndex - 25), firstIndex + 25));
-            } else {
-                // console.log("First name:", input, "=>", firstName,"=", expected.first);
-            }
-            if (lastName !== expected.last) {
-                console.log("Mismatch:", input, expected.last, lastName);
-                console.log(lastNames.slice(Math.max(0, lastIndex - 25), lastIndex + 25));
-            }
+        if (firstName !== expected.first) {
+            console.log("Mismatch:", input, "expected =", expected.first, "actual =", firstName);
+            console.log(firstNames.slice(Math.max(0, firstIndex - 25), firstIndex + 25));
+        } else {
+            // console.log("First name:", input, "=>", firstName,"=", expected.first);
         }
-    });
-
-
-})();
+        if (lastName !== expected.last) {
+            console.log("Mismatch:", input, expected.last, lastName);
+            console.log(lastNames.slice(Math.max(0, lastIndex - 25), lastIndex + 25));
+        }
+    }
+});
